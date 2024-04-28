06cedf195ddb083aeae5e4b6c3ad600082c882e6
Shuyou <shuyou.zhan@zilliz.com>
2024-04-28 18:53:36 +0800

    luhn

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,4 +1,31 @@
-/// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    todo!("Is the Luhn checksum for {code} valid?");
+    if code.trim() == "0" {
+        return false;
+    }
+
+    let nums = code
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .map(|c| c.to_digit(10))
+        .collect::<Option<Vec<_>>>();
+
+    nums.map_or(false, |nums| {
+        nums.iter()
+            .rev()
+            .enumerate()
+            .map(|(i, &n)| {
+                if i % 2 == 0 {
+                    n
+                } else {
+                    let mut n = n * 2;
+                    if n > 9 {
+                        n -= 9;
+                    }
+                    n
+                }
+            })
+            .sum::<u32>()
+            % 10
+            == 0
+    })
 }
