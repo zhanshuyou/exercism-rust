03bc841e3241ace837271bb6f942eaef9a2868a1
Shuyoou <shuyoou@outlook.com>
2024-05-07 08:25:55 +0800

    feat: difference of squares done

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,11 +1,12 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    todo!("square of sum of 1...{n}")
+    let sum: u32 = (1..=n).sum();
+    sum.pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    todo!("sum of squares of 1...{n}")
+    (1..=n).map(|i| i.pow(2)).sum()
 }
 
 pub fn difference(n: u32) -> u32 {
-    todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    square_of_sum(n) - sum_of_squares(n)
 }
