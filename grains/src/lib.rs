b91f7a8643574032016e2580fb987c3f2eadb715
Shuyou <shuyoou@outlook.com>
2025-02-11 15:09:10 +0800

    feat: grain done

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,15 @@
 pub fn square(s: u32) -> u64 {
-    todo!("grains of rice on square {s}");
+    if s == 0 || s > 64 {
+        panic!("Square must be between 1 and 64");
+    }
+
+    u64::pow(2, s - 1)
 }
 
 pub fn total() -> u64 {
-    todo!();
+    let mut sum: u64 = 0;
+    for i in 1..=64 {
+        sum += square(i);
+    }
+    sum
 }
---
+++ leap/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn is_leap_year(year: u64) -> bool {
+    todo!("true if {year} is a leap year")
+}
