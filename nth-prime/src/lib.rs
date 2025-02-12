c8d032b1f84e0f9684d1caaf758fe88a24b0992c
Shuyou <shuyoou@outlook.com>
2025-02-12 14:16:22 +0800

    feat: nth-prime done

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn nth(n: u32) -> u32 {
-    todo!("What is the 0-indexed {n}th prime number?")
+    let mut primes = vec![2];
+    let mut candidate = 3;
+    while primes.len() <= n as usize {
+        if primes.iter().all(|prime| candidate % prime != 0) {
+            primes.push(candidate);
+        }
+        candidate += 2;
+    }
+    primes[n as usize]
 }
