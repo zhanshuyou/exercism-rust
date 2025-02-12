99d7d19c11c03f07cf7c22444a76bfe9e147efbf
Shuyou <shuyoou@outlook.com>
2025-02-12 14:38:19 +0800

    feat: prime-factors done

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    todo!("This should calculate the prime factors of {n}")
+    let mut factors = Vec::new();
+    let mut n = n;
+    let mut candidate = 2;
+    while n > 1 {
+        while n % candidate == 0 {
+            factors.push(candidate);
+            n /= candidate;
+        }
+        candidate += 1;
+    }
+    factors
 }
